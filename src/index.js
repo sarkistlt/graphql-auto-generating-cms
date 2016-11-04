44f7ddb71940cc069b8ba6ca07426c672a2bd5ff
Sarkis <sarkis.tlt@gmail.com>
2016-11-04 11:23:27 -0700

    scss issue [fixed]

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import {Grid, Loader, Segment, Message} from 'semantic-ui-react';
 import SideMenu from './components/SideMenu';
 import View from './components/View';
 import List from './components/List';
-import './components/styles.scss';
+import './components/styles.css';
 
 export default class Layout extends Component {
     constructor(props) {
