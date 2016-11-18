c149eaadba07368061825764ff4a55162dcd6c90
Sarkis <sarkis.tlt@gmail.com>
2016-11-18 10:30:09 -0800

    example with two custom components

--- example/src/app.js
+++ example/src/app.js
@@ -3,40 +3,58 @@ import React from 'react';
 import {render} from 'react-dom';
 //import {Router, Route, browserHistory} from 'react-router';
 
-class View extends React.Component {
+class firstComponent extends React.Component {
     render() {
         return (
-            <div>place for your custom solution</div>
+            <div>place for your first custom solution</div>
         );
     }
 }
 
-let newItem = {
-    label: 'Custom component',
-    secret: 'newItem',
-    view: {
-        secret: 'newItem',
-        component: View
+class secondComponent extends React.Component {
+    render() {
+        return (
+            <div>place for your second custom solution</div>
+        );
     }
-};
+}
+
+let customComponents = [
+    {
+        label: 'Custom component 1',
+        secret: 'firstComponent',
+        view: {
+            secret: 'firstComponent',
+            component: firstComponent
+        }
+    },
+    {
+        label: 'Custom component 2',
+        secret: 'secondComponent',
+        view: {
+            secret: 'secondComponent',
+            component: secondComponent
+        }
+    }
+];
 
 render(
     <GraphqlCMS
         endpoint='/graphql_cms_endpoint'
         graphql='/graphql'
-        newMenuItems={[newItem]}
+        newMenuItems={customComponents}
     />, document.getElementById('root'));
 
 /**
  * if you want to try on separate route
  */
 /*render(
-    <Router onUpdate={() => window.scrollTo(0, 0)} history={browserHistory}>
-        <Route
-            path='/admin'
-            endpoint='/graphql_cms_endpoint'
-            graphql='/graphql'
-            newMenuItems={[newItem]}
-            components={GraphqlCMS}
-        />
-    </Router>, document.getElementById('root'));*/
+ <Router onUpdate={() => window.scrollTo(0, 0)} history={browserHistory}>
+ <Route
+ path='/admin'
+ endpoint='/graphql_cms_endpoint'
+ graphql='/graphql'
+ newMenuItems={[newItem]}
+ components={GraphqlCMS}
+ />
+ </Router>, document.getElementById('root'));*/
